b882f1fc6cfb2a1ca733848dc2ed1f32c288fef5
MUHAMAD IQBAL FATUROHMAN <iqbalfatur@365.telkomuniversity.ac.id>
2022-03-26 17:07:35 +0700

    penambahan design by contract pada semua kelas

--- modul5_1302200016/SayaTubeUser.cs
+++ modul5_1302200016/SayaTubeUser.cs
@@ -14,33 +14,63 @@ namespace modul5_1302200016
 
         public SayaTubeUser(string username)
         {
+            SetUsername(username);
             this.Username = username;
             this.id = new Random().Next(10000, 99999);
             this.uploadedVideos = new List<SayaTubeVideo>();
         }
 
+        public void SetUsername(string username)
+        {
+            // Menambahkan if apabila parameternya null dan panjang karakter lebih dari 100
+            if (username == null) return;
+            if (username.Length == 0) return;
+            if (username.Length > 100) return;
+
+            this.Username = username;
+        }
         public int GetTotalVideoPlayCount()
         {
             int count = 0;
 
+            // Melakukan looping
             foreach (SayaTubeVideo video in this.uploadedVideos)
             {
-                count += video.GetPlayCount();
+                try
+                {
+                    count = checked(count + video.GetPlayCount());
+                }catch (Exception g)
+                {
+                    Console.WriteLine(g.Message);
+                    break;
+                }
             }
             return count;
         }
 
         public void AddVideo(SayaTubeVideo video)
         {
-            this.uploadedVideos.Add(video);
+            // menambahkan if apabila parameter video null
+            if (video == null) return;
+            
+            if (video.GetPlayCount() > int.MaxValue)
+            {
+                this.uploadedVideos.Add(video);
+            }
         }
 
         public void PrintAllVideoPlayCount()
         {
             int nomor = 1;
             Console.WriteLine("User: " + this.Username);
+
+            //melakukan Looping 
             foreach (SayaTubeVideo video in this.uploadedVideos)
             {
+               // menambahakan if apabila nomor lebih dari 8
+                if (nomor > 8) break;
+
+                // menampilkan ketika nomor kurang dari 8
                 Console.WriteLine("Video " + nomor + ": " + video.GetJudul());
                 nomor++;
             }
--- modul5_1302200016/SayaTubeVideo.cs
+++ modul5_1302200016/SayaTubeVideo.cs
@@ -16,16 +16,39 @@ namespace modul5_1302200016
         public SayaTubeVideo(string title)
         {
             this.title = title;
+            SetJudul(title);
             this.id = new Random().Next(10000, 99999);
             this.playCount = 0;
+        }
+
+        public void SetJudul(string title)
+        {
+            // menambahkan if apabila judulnya null dan jumlah karakter kurang dari 200
+            if (title == null) return;
+
+            if (title.Length > 0) return;
+            if (title.Length > 200) return;
+
+            this.title = title;
         }
 
         //Menambahkan playCount dengan parameter "tambah"
         public void IncreasePlayCount(int tambah)
         {
-            this.playCount += tambah;
+            playCount += tambah;
+            //Menambahkan if apabila parameter negatif dan melebihi 25.000.000
+            if (tambah < 0) return;
+            if (tambah > 25000000) return;
+
+            try
+            {
+                playCount = checked(playCount + tambah);
+            }catch(Exception g)
+            {
+                Console.WriteLine(g.Message);
+            }
         }
-        // menambahkan informasi video (ID, Judul, Putar)
+        // menampilkan detail video
         public void PrintVideoDetails()
         {
             Console.WriteLine("ID : " + id + ", Judul: " + title + ", Putar : " + playCount);
